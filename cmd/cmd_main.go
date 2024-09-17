1a85456e46fa6cff293b8a384b4af8d6e8426532
mierdi <kharkavin.vd>
2024-09-17 09:38:29 +0800

    add default behavior

--- cmd/cmd_main.go
+++ cmd/cmd_main.go
@@ -1,5 +1,22 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"shelve/git"
 
-var MainCommand = &cobra.Command{}
+	"github.com/spf13/cobra"
+)
+
+var MainCommand = &cobra.Command{
+	Run: start,
+}
+
+func start(cmd *cobra.Command, args []string) {
+	if l, err := git.CreateStash(""); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else if len(l) > 0 {
+		fmt.Println(l)
+	}
+}
